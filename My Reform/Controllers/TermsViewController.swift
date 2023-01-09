97fb121b08315bc3db106b6641af5cce245d481f
Hyeonjoonn <phj3046@gmail.com>
2023-01-09 14:53:51 +0900

    loading

---
+++ My Reform/Controllers/TermsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TermsViewController.swift
+//  My Reform
+//
+//  Created by 박현준 on 2023/01/09.
+//
+
+import UIKit
+
+class TermsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ My Reform/Views/TermsCheckBox.swift
@@ -0,0 +1,20 @@
+//
+//  TermsCheckBox.swift
+//  My Reform
+//
+//  Created by 박현준 on 2023/01/09.
+//
+
+import UIKit
+
+class TermsCheckBox: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
