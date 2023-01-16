dec23ff998faf110471a14c35e4d74635337283a
Hyeonjoonn <phj3046@gmail.com>
2023-01-16 15:23:32 +0900

    0116 first Manager and Model add

---
+++ My Reform/Network/Managers/Users/LoginSignUpDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  LoginSignUpDataManager.swift
+//  My Reform
+//
+//  Created by 박현준 on 2023/01/16.
+//
+
+import Foundation
---
+++ My Reform/Network/Model/Login/LoginInput.swift
@@ -0,0 +1,13 @@
+//
+//  LoginInput.swift
+//  My Reform
+//
+//  Created by 박현준 on 2023/01/16.
+//
+
+import Foundation
+
+struct LoginInput: Encodable{
+    var id: String?
+    var password: String?
+}
